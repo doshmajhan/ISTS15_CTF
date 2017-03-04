36d04a6dd5243c9c7ad63bae8c99ad163d8aaa14
Dave Kukfa <dxk2652@rit.edu>
2017-03-03 22:24:57 -0500

    Create sendflag.sh

---
+++ web/200/sendflag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while [ 1 ]
+do
+	curl -k --referer 'flag{lik3_a_surg30n_cutt1n_f0r_th3_v3ry_f1rs7_t1m3}' --cookie 'flag{lik3_a_surg30n_cutt1n_f0r_th3_v3ry_f1rs7_t1m3}' --user-agent 'flag{lik3_a_surg30n_cutt1n_f0r_th3_v3ry_f1rs7_t1m3}' -H 'Flag: flag{lik3_a_surg30n_cutt1n_f0r_th3_v3ry_f1rs7_t1m3}' -H 'Flag: flag{lik3_a_surg30n_cutt1n_f0r_th3_v3ry_f1rs7_t1m3}' https://localhost/ > /dev/null 2>&1
+	sleep 5
+done
